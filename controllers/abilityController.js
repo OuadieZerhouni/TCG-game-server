0df661255b477649325950dd52701e6fd04c969d
OuadieZerhouni <ouadie2zerhouni@gmail.com>
2024-04-11 13:47:16 +0000

    testing

--- controllers/abilityController.js
+++ controllers/abilityController.js
@@ -1,146 +1,154 @@
-/* Author: Ouadie ZERHOUNI
-   Creation Date: 2024-01-28 01:22:07 */
+// /* Author: Ouadie ZERHOUNI
+//    Creation Date: 2024-01-28 01:22:07 */
 
+// const fs = require("fs");
+// const path = require("path");
+// const util = require("util");
+// const AbilityService = require("../services/abilityService");
+// const writeFileAsync = util.promisify(fs.writeFile);
 
+// /**
+//  * Controller for managing ability operations.
+//  */
+// class AbilityController {
+//   /**
+//    * Get a list of all abilities.
+//    *
+//    * @param {express.Request} req - The request object.
+//    * @param {express.Response} res - The response object.
+//    * @returns {Promise<void>}
+//    */
+//   static async getAllAbilities(req, res) {
+//     try {
+//       const abilities = await AbilityService.getAllAbilities();
+//       res.status(200).json(abilities);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send("Internal Server Error");
+//     }
+//   }
 
-const AbilityService = require('../services/abilityService');
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
-const writeFileAsync = util.promisify(fs.writeFile);
+//   /**
+//    * Get an ability by ID.
+//    *
+//    * @param {express.Request} req - The request object.
+//    * @param {express.Response} res - The response object.
+//    * @returns {Promise<void>}
+//    */
+//   static async getAbilityById(req, res) {
+//     try {
+//       const abilityId = req.params.abilityId;
+//       const ability = await AbilityService.findAbilityById(abilityId);
+//       if (!ability) {
+//         res.status(404).send("Ability not found");
+//       } else {
+//         res.status(200).json(ability);
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send("Internal Server Error");
+//     }
+//   }
 
+//   /**
+//    * Create a new ability.
+//    *
+//    * @param {express.Request} req - The request object.
+//    * @param {express.Response} res - The response object.
+//    * @returns {Promise<void>}
+//    */
+//   static async createAbility(req, res) {
+//     try {
+//       // Access the fields sent in the request
+//       const { functionName, power } = req.body;
+//       // Create an ability object with power and functionName
+//       const ability = {
+//         power,
+//         functionName,
+//       };
 
-/**
- * Controller for managing ability operations.
- */
-class AbilityController {
-  /**
-   * Get a list of all abilities.
-   *
-   * @param {express.Request} req - The request object.
-   * @param {express.Response} res - The response object.
-   * @returns {Promise<void>}
-   */
-  static async getAllAbilities(req, res) {
-    try {
-      const abilities = await AbilityService.getAllAbilities();
-      res.status(200).json(abilities);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-  }
+//       const createdAbility = await AbilityService.createAbility(ability);
 
-  /**
-   * Get an ability by ID.
-   *
-   * @param {express.Request} req - The request object.
-   * @param {express.Response} res - The response object.
-   * @returns {Promise<void>}
-   */
-  static async getAbilityById(req, res) {
-    try {
-      const abilityId = req.params.abilityId;
-      const ability = await AbilityService.findAbilityById(abilityId);
-      if (!ability) {
-        res.status(404).send('Ability not found');
-      } else {
-        res.status(200).json(ability);
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-  }
+//       // Save the image data as a PNG file with the function name as the filename
+//       if (req.file) {
+//         // Remove the data URI prefix and decode the base64 string
+//         const base64Image = req.file.buffer
+//           .toString("utf8")
+//           .replace(/^data:image\/\w+;base64,/, "");
+//         const imageBuffer = Buffer.from(base64Image, "base64");
 
-/**
- * Create a new ability.
- *
- * @param {express.Request} req - The request object.
- * @param {express.Response} res - The response object.
- * @returns {Promise<void>}
- */
-static async createAbility(req, res) {
-  try {
-    // Access the fields sent in the request
-    const { functionName, power } = req.body;
-    // Create an ability object with power and functionName
-    const ability = {
-        power,
-        functionName,
-    };
+//         const imagePath = path.join(
+//           __dirname,
+//           `../data/abilities/${functionName}.png`
+//         );
 
-    const createdAbility = await AbilityService.createAbility(ability);
+//         // Write the decoded buffer to a file
+//         await writeFileAsync(imagePath, imageBuffer);
 
-    // Save the image data as a PNG file with the function name as the filename
-    if (req.file) {
-      // Remove the data URI prefix and decode the base64 string
-      const base64Image = req.file.buffer.toString('utf8').replace(/^data:image\/\w+;base64,/, '');
-      const imageBuffer = Buffer.from(base64Image, 'base64');
-  
-      const imagePath = path.join(__dirname, `../data/abilities/${functionName}.png`);
-  
-      // Write the decoded buffer to a file
-      await writeFileAsync(imagePath, imageBuffer);
-  
-      // Include the image path in the response if needed
-      createdAbility.image = imagePath;
-  }
+//         // Include the image path in the response if needed
+//         createdAbility.image = imagePath;
+//       }
 
-    res.status(201).json(createdAbility);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-}
+//       res.status(201).json(createdAbility);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send("Internal Server Error");
+//     }
+//   }
 
-  /**
-   * Update an ability by ID.
-   *
-   * @param {express.Request} req - The request object.
-   * @param {express.Response} res - The response object.
-   * @returns {Promise<void>}
-   */
-  static async updateAbility(req, res) {
-    try {
-      const abilityId = req.params.abilityId;
-      const updatedData = req.body;
-      const updatedAbility = await AbilityService.updateAbility(abilityId, updatedData);
-      if (!updatedAbility) {
-        res.status(404).send('Ability not found');
-      } else {
-        res.status(200).json(updatedAbility);
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-  }
+//   /**
+//    * Update an ability by ID.
+//    *
+//    * @param {express.Request} req - The request object.
+//    * @param {express.Response} res - The response object.
+//    * @returns {Promise<void>}
+//    */
+//   static async updateAbility(req, res) {
+//     try {
+//       const abilityId = req.params.abilityId;
+//       const updatedData = req.body;
+//       const updatedAbility = await AbilityService.updateAbility(
+//         abilityId,
+//         updatedData
+//       );
+//       if (!updatedAbility) {
+//         res.status(404).send("Ability not found");
+//       } else {
+//         res.status(200).json(updatedAbility);
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send("Internal Server Error");
+//     }
+//   }
 
-  /**
-   * Delete an ability by ID.
-   *
-   * @param {express.Request} req - The request object.
-   * @param {express.Response} res - The response object.
-   * @returns {Promise<void>}
-   */
-  static async deleteAbility(req, res) {
-    try {
-      const abilityId = req.params.abilityId;
-      const deleted = await AbilityService.deleteAbility(abilityId);
-      if (!deleted) {
-        res.status(404).send('Ability not found');
-      } else {
-        // delete the image file
-        const imagePath = path.join(__dirname, `../data/abilities/${deleted.functionName}.png`);
-        fs.unlinkSync(imagePath);
-        res.status(204).send('Ability deleted successfully');
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-  }
-}
+//   /**
+//    * Delete an ability by ID.
+//    *
+//    * @param {express.Request} req - The request object.
+//    * @param {express.Response} res - The response object.
+//    * @returns {Promise<void>}
+//    */
+//   static async deleteAbility(req, res) {
+//     try {
+//       const abilityId = req.params.abilityId;
+//       const deleted = await AbilityService.deleteAbility(abilityId);
+//       if (!deleted) {
+//         res.status(404).send("Ability not found");
+//       } else {
+//         // delete the image file
+//         const imagePath = path.join(
+//           __dirname,
+//           `../data/abilities/${deleted.functionName}.png`
+//         );
+//         fs.unlinkSync(imagePath);
+//         res.status(204).send("Ability deleted successfully");
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send("Internal Server Error");
+//     }
+//   }
+// }
 
-module.exports = AbilityController;
+// module.exports = AbilityController;
